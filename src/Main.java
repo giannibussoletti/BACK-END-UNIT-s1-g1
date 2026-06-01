b9b391dffd307e7fc54674f5178812830cd180bc
giannibussoletti <bussolettigianni@gmail.com>
2026-06-01 12:19:07 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,14 @@ public class Main {
         System.out.println("Hello World");  // Shortcut -< sout
 
         final double DICHIARAZIONE_CONSTANTE = 10.4; // con la parola final si definisce una costante in Java, e si usa lo snake case tutto in maiuscolo
+        float numeroDecimale2 = 10.3f; // Sebbene i float occupino metà dello spazio rispetto
+        // ai double, è meglio usare SEMPRE I DOUBLE in quanto i processori moderni li usano
+        // più efficientemente per i calcoli
+        //-----------------------------STRINGHE----------------------
+        //Non vengono considerate dati primitivi, perché sono praticamente delle Classi.
+        String text = "Hello world!"; //<-- le stringhe hanno sempre gli apici doppi
+        System.out.println(text);
 
     }
+
 }
